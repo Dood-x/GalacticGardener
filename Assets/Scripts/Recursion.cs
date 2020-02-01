ba14c30a669ae8589e9cd3adb5b66c9e434509f8
Aduel13 <adrianpavelic@gmail.com>
2020-02-01 14:14:49 +0100

    ye

---
+++ Assets/Scripts/Recursion.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recursion : MonoBehaviour
+{
+    void recursion(int depth)
+    {
+        recursion(depth + 1);
+    }
+}
+
