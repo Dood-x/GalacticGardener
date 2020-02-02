b00c9feae186e78f52b92aa4104545077a60cf3a
Aduel13 <adrianpavelic@gmail.com>
2020-02-02 03:16:38 +0100

    ye

---
+++ Assets/Scripts/FuelDraining.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelDraining : MonoBehaviour
+{
+    public Slider FuelBar;
+    public float fuel;
+    public float MaxFuel;
+    float speed = 0.5f;
+
+     void Start()
+    {
+        fuel = MaxFuel;
+        FuelBar = GetComponent<Slider>();
+        FuelBar.maxValue = MaxFuel;
+        FuelBar.value = fuel;
+
+    }
+
+    void Update()
+    {
+        fuel -= Time.deltaTime * speed;
+        FuelBar.value = fuel;
+    }
+}
