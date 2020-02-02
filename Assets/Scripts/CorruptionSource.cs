c1d231f215a1f57c53f944ec3c9eb7c87716b203
Aduel13 <adrianpavelic@gmail.com>
2020-02-02 01:22:45 +0100

    ye

--- Assets/Scripts/CorruptionSource.cs
+++ Assets/Scripts/CorruptionSource.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 public class CorruptionSource : MonoBehaviour
 {
+    public List<Corruption> allcorupted;
+
     public float regenTime;
     
     public int startDepth;
+
+    private bool IsAllMissionComplete()
+    {
+        for (int i = 0; i < allcorupted.Count; ++i)
+        {
+            if (allcorupted[i].healed == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+
+
+
 
 }
